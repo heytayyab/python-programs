840a3a69bd7fbb918bd4b9b14a7ef1b6801d5608
heytayyab <123201236+heytayyab@users.noreply.github.com>
2023-03-04 15:11:26 +0500

    Added demo file of python

---
+++ demo.py
@@ -0,0 +1,9 @@
+#write a program to take two numbers from user and print the sum of two numbers
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The sum of two numbers is: ", a+b)
+
+# q: what is oop?
+# a: oop is a programming paradigm that uses objects and their interactions to design and program applications.
+# q: what is object?
+# a: object is a real world entity that has some attributes and behavior.
